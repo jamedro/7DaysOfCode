3948ddb10a0f20b6c35fef1c0b9c6953f98a150b
jamedro <jamedro09@hotmail.com>
2025-03-03 17:49:47 -0500

    actualiza 7dayOfcode-3.js

--- 7dayOfcode-3.js
+++ 7dayOfcode-3.js
@@ -1,5 +1,3 @@
-
-
 let numeroAdivinar = 9
 let numeroSugerido = prompt("Hola buenas, favor ingrese un numero del 1 al 10")
 let intentos = 0;
@@ -11,11 +9,12 @@ while (numeroSugerido != numeroAdivinar) {
     numeroSugerido = prompt (`Me indicas un número diferente por favor, llevas ${intentos} intentos fallidos`)
 }
 
+intentos++;
+console.log(intentos);
+
 if (numeroSugerido == numeroAdivinar) {
     alert(`Que bien, si era el número ${numeroAdivinar} que indicaste`)
 }
-intentos++;
-console.log(intentos);
 
 
 
