ad191ae34d5ca9260d68880d6f7d1cbb0b2671d7
jamedro <jamedro09@hotmail.com>
2025-03-03 10:18:47 -0500

    Add files via upload

---
+++ 7dayOfcode-2.js
@@ -0,0 +1,31 @@
+
+
+let nombre = prompt("Ingrese su nombre");
+let edad = prompt("Ingrese su edad");
+let estudios = prompt("Ingrese que lenguaje de programación estudia actualmente");
+let datosCompletos = `Hola ${nombre}, Tienes ${edad} años, y estas aprendiendo ${estudios}`;
+alert(datosCompletos);
+
+let teGusta = `Te gusta estudiar ${estudios}?`;	
+alert(teGusta);   
+
+let respuesta = prompt("Responde con 1 para Si, ó 2 para No");
+if (respuesta == 1) {
+  alert('¡Muy bien! Sigue estudiando y tendrás mucho éxito.');
+} else {    
+  alert('Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?');
+}   
+
+let despedida = `¡Hasta luego ${nombre}!`;
+alert(despedida);
+
+// Output:
+// Hola Juan, Tienes 25 años, y estas aprendiendo JavaScript    
+// Te gusta estudiar JavaScript?
+// Responde con 1 para Si, ó 2 para No
+// 1
+// ¡Muy bien! Sigue estudiando y tendrás mucho éxito.
+// ¡Hasta luego Juan!
+
+
+ 
---
+++ 7dayOfcode-3.js
@@ -0,0 +1,21 @@
+let numeroAdivinar = 9
+let numeroSugerido = prompt("Hola buenas, favor ingrese un numero del 1 al 10")
+let intentos = 0
+numeroSugerido = Number(numeroSugerido);
+console.log (numeroSugerido);
+
+
+while (numeroSugerido != numeroAdivinar) {
+    numeroSugerido = prompt ("Me indicas un númro diferente por favor")
+}
+
+if (numeroSugerido == numeroAdivinar) {
+    alert(`Que bien, es el número ${numeroAdivinar} que acertaste`)
+}
+intentos++;
+
+
+
+
+
+
